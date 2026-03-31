22ac62f4fd3a00592e3e2949fc96ee36590076a6
Raul PR <elprofe153@gmail.com>
2026-03-31 11:40:39 +0700

    Add build/ to gitignore

--- build/blocks/Widget/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '7a304f8cf069895b65d8');
